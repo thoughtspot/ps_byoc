9fc6ec29efa9c2ebdf09326bf01f836adfb46a8d
harshmeet.singh <harshmeet.singh@thoughtspot.com>
2024-10-10 16:54:26 +0530

    make another url for heatmapchart

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                kpi_comparison_chart: resolve(__dirname, 'kpi_comparison_chart/index.html'),
-                main: resolve(__dirname, 'index.html'),
-            }
-        }
-    }
+  build: {
+    rollupOptions: {
+      input: {
+        kpi_comparison_chart: resolve(
+          __dirname,
+          'kpi_comparison_chart/index.html'
+        ),
+        heatmap_chart: resolve(__dirname, 'heatmap_chart/index.html'),
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
